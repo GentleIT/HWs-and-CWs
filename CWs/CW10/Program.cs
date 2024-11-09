d473d434f048723299e9eff6996153fbed7fb18a
GentleIT <69665800+GentleIT@users.noreply.github.com>
2024-11-09 23:05:58 +0500

    Чертовы делегаты

---
+++ CWs/CW10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Program
+{
+    class Program
+    {
+        public delegate string DecisionDelegate(string parameter);
+
+        public static void ProcessDecision(DecisionDelegate decisionMaker)
+        {
+            Console.WriteLine("Введите политическое решение:");
+            string input = Console.ReadLine() ?? "";
+            Console.WriteLine("Результат обработки: " + decisionMaker(input));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя и срок: ");
+            string name = Console.ReadLine() ?? "";
+            int term = int.Parse(Console.ReadLine() ?? "");
+
+            USPresident president = new USPresident(name, term);
+
+            DecisionDelegate decisionDelegate = new(USPresident.MakeDecision);
+
+
+            Console.WriteLine("Хотите ли задать политическое решение? \n [0] Нет \n [1] Да");
+            int questionOfDecision = int.Parse(Console.ReadLine() ?? "");
+
+            if (questionOfDecision == 1)
+            {
+                ProcessDecision(decisionDelegate);
+            }
+        }
+    }
+}
---
+++ CWs/CW10/USPresident.cs
@@ -0,0 +1,41 @@
+using System.Security.Cryptography.X509Certificates;
+
+namespace Program
+{
+    // public delegate string DecisionDelegate(string parameter);
+
+    public class USPresident
+    {
+        private string _name;
+        private int _term;
+
+        public USPresident(string _name, int _term)
+        {
+            this._name = _name;
+            SetTerm(_term);
+        }
+
+        /* SetTerm(int years) – метод для установки срока президентства. Если значение срока 
+        больше 8 лет (максимум для двух сроков), выбросьте исключение InvalidOperationException 
+        с сообщением «Срок превышает допустимое количество лет для президентства». */
+        public void SetTerm(int years)
+        {
+            if (years > 8)
+            {
+                throw new InvalidOperationException("Срок превышает допустимое количество лет для президентства");
+            }
+            else
+            {
+                this._term = years;
+            }
+        }
+
+        // MakeDecision(string decision) – метод, возвращающий строку с сообщением о принятом решении.
+        // Ну, возвращаю.
+        public static string MakeDecision(string decision)
+        {
+            Console.WriteLine("Было принято новое политическое решение! ");
+            return decision;
+        }
+    }
+}
