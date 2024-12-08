c0910e55e833c5a1c0d6a778b47cddd0cdb00623
GentleIT <69665800+GentleIT@users.noreply.github.com>
2024-12-08 16:58:27 +0500

    Week 14

---
+++ HWs/Week 14/4. Dictionary Студенттердің бағаларын есептеу.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, int> studentGrades = new Dictionary<string, int>();
+        studentGrades.Add("Eldos", 90);
+        studentGrades.Add("Nurzhan", 85);
+        studentGrades.Add("Nurlan", 80);
+        studentGrades.Add("Bekzat", 75);
+
+        Console.Write("Бағасын білу үшін студенттің атын енгізіңіз: ");
+        string name = Console.ReadLine() ?? " ";
+
+        if (studentGrades.TryGetValue(name, out int grade))
+        {
+            Console.WriteLine($"{name} студентінің бағасы: {grade}");
+        }
+        else
+        {
+            Console.WriteLine("Мұндай студент табылмады.");
+        }
+    }
+}
